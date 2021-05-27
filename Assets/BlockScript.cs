3c930f64a0f303470033cc89e6de8ad44800db11
Varka <macma1702@gmail.com>
2021-05-27 13:16:16 -0500

    simple movement and simple input

---
+++ Assets/BlockScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public GameObject spawnPoint;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("blockSpawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (gameObject.GetComponent<Rigidbody2D>() == null)
+                {
+                    SetRigi();                    
+                    transform.parent = null;
+                    Invoke("GNB",1f);                    
+                }
+                else
+                {
+                    return;
+                }
+            }      
+        
+    }
+
+    public void SetRigi()
+    {
+        gameObject.AddComponent<Rigidbody2D>();
+        GetComponent<Rigidbody2D>().gravityScale = 2;
+        GetComponent<Rigidbody2D>().drag = 1;
+        GetComponent<Rigidbody2D>().angularDrag = 1;
+    }
+
+    public void GNB()
+    {
+        spawnPoint.GetComponent<hasBlock>().GenerateNewBlock();
+    }
+
+   
+}
---
+++ Assets/hasBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hasBlock : MonoBehaviour
+{
+    
+    public GameObject blockObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+
+
+    public void GenerateNewBlock()
+    {
+        GameObject myBlock = Instantiate(blockObj, transform.position, Quaternion.identity);
+        myBlock.transform.parent = gameObject.transform;
+    }
+}
