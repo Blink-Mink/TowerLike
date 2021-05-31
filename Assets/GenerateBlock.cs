01ebf48c25b63f0c4174bf18b04323e73ad13719
Varka <macma1702@gmail.com>
2021-05-31 13:32:02 -0500

    fixed multiple block updates

--- Assets/GenerateBlock.cs
+++ Assets/GenerateBlock.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class GenerateBlock : MonoBehaviour
 {
-    
     [SerializeField] private GameObject blockObj;
+    
     private Transform spawnPoint;
-    GameObject lastBlock;
-   
+    private Transform pendulo;
+    private GameObject lastBlock;
+    private bool doInput;
+
     void Start()
     {
         spawnPoint = GameObject.FindGameObjectWithTag("blockSpawn").transform;
+        pendulo = GameObject.FindGameObjectWithTag("pendulo").transform;
         GenerateNewBlock();
-    }
-    
+    }   
 
 
     public void EnableBlockInput()
     {
-        lastBlock.GetComponent<BlockScript>().doInput = true;
+        doInput = true;
     }
 
     public void DisableBlockInput()
     {
-        lastBlock.GetComponent<BlockScript>().doInput = false;
+        doInput = false;
     }
 
     public void GenerateNewBlock()
@@ -32,5 +34,40 @@ public class GenerateBlock : MonoBehaviour
         GameObject myBlock = Instantiate(blockObj, spawnPoint.position, spawnPoint.rotation);
         myBlock.transform.parent = spawnPoint.transform;
         lastBlock = myBlock;
+    }
+
+
+    void Update()
+    {
+
+        if (doInput)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (lastBlock != null)
+                {
+                    if (lastBlock.gameObject.GetComponent<Rigidbody2D>() == null)
+                    {
+                        SetRigi();
+                        lastBlock.transform.parent = null;
+                        pendulo.gameObject.GetComponent<ControlCuerda>().DoCraneOut = true;
+                        doInput = false;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }                
+            }
+        }
+
+    }
+
+    public void SetRigi()
+    {
+        lastBlock.AddComponent<Rigidbody2D>();
+        lastBlock.GetComponent<Rigidbody2D>().gravityScale = 2;
+        lastBlock.GetComponent<Rigidbody2D>().drag = 1;
+        lastBlock.GetComponent<Rigidbody2D>().angularDrag = 1;
     }
 }
